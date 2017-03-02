6644ea23e7b7f2ece857bfedf16e3dc3c423bf57
Benjamin Krier <benkrier@gmail.com>
2017-03-02 16:52:42 -0600

    Add delete option

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -26,7 +26,13 @@ class MoviesController < ApplicationController
 		@movie.save
 		redirect_to @movie
 	end
-	
+
+	def destroy
+		@movie = Movie.find(params[:id])
+		@movie.destroy
+		redirect_to movies_url
+	end
+
 private
 
 	def movie_params
